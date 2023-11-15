ce597514bee2aa611b33c31d5bdadca923ee8b95
Nhi Yen <88694623+yennhi95zz@users.noreply.github.com>
2023-11-15 14:09:13 +0000

    update code

--- ecom_chatbot.py
+++ ecom_chatbot.py
@@ -3,22 +3,3 @@ from openai import OpenAI
 from IPython.display import display
 import ipywidgets as widgets
 
-
-
-# from dotenv import load_dotenv
-# load_dotenv()
-
-# import os
-# from dotenv import Dotenv
-# dotenv = Dotenv(os.path.join(os.path.dirname(__file__), ".env")) # Of course, replace by your correct path
-# os.environ.update(dotenv)
-
-# from python-dotenv import load_dotenv
-
-# load_dotenv()
-
-import dotenv
-# print(dir(dotenv))
-
-dotenv_path = join(dirname(__file__), '.env')
-load(dotenv_path)
